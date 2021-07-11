6a08a28ad127538482c59530e529b6b26242bbd5
Wesley Camargo <camargo.wes@gmail.com>
2021-07-11 23:44:22 +0200

    initial commit

---
+++ dotnetcore/src/dotnetcoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetcoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
